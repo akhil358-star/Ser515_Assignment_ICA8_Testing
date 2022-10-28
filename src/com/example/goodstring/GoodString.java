09851e2b2c35e3f09f95c3915fd69494df71fb58
akhil358-star <akhilsai358@gmail.com>
2022-10-27 21:11:47 -0700

    Skeleton code

---
+++ src/com/example/goodstring/GoodString.java
@@ -0,0 +1,13 @@
+package com.example.goodstring;
+public class GoodString {
+        static Boolean goodString( ) {
+            System.out.println("Not yet implemented");
+            return true;
+        }
+
+        public static void main(String[] args) {
+            goodString();
+    }
+    }
+
+
